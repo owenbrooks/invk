fb64da07cc50fd22f292c660bfa32dc179148a86
Owen Brooks <owen.h.brooks@gmail.com>
2021-06-21 17:46:11 +1000

    Correct angle addition

--- src/bin/direct.rs
+++ src/bin/direct.rs
@@ -15,15 +15,23 @@ fn model(_app: &App) -> Model {
         length: 100.,
         angle: 0.,
     };
+    let l3 = Link {
+        length: 80.,
+        angle: 0.,
+    };
+    let l4 = Link {
+        length: 50.,
+        angle: 0.,
+    };
     Model {
-        links: vec![l1, l2],
+        links: vec![l1, l2, l3, l4],
     }
 }
 fn event(_app: &App, model: &mut Model, event: Event) {
     match event {
         Event::Update(update) => {
             let time = update.since_start.as_secs_f32();
-            let scale_factor = 0.1;
+            let scale_factor = 1.;
             let new_angle = time * scale_factor % std::f32::consts::TAU;
             for link in &mut model.links {
                 link.angle = new_angle;
@@ -42,16 +50,18 @@ fn view(app: &App, model: &Model, frame: Frame) {
     // Draw links
     let width = 30.;
     let extra = 5.;
-    let mut next_joint = pt2(0., -300.);
+    let mut next_joint = pt2(0., 0.);
+    let mut angle_sum = 0.;
     for link in &model.links {
-        let centre_x = (link.length / 2. - extra) * link.angle.cos();
-        let centre_y = (link.length / 2. - extra) * link.angle.sin();
+        angle_sum += link.angle;
+        let centre_x = (link.length / 2. - extra) * angle_sum.cos();
+        let centre_y = (link.length / 2. - extra) * angle_sum.sin();
         draw.ellipse()
             .w_h(link.length + extra, width)
             .color(GREENYELLOW)
             .stroke(DARKBLUE)
             .stroke_weight(1.)
-            .z_radians(link.angle)
+            .z_radians(angle_sum)
             .x_y(next_joint.x + centre_x, next_joint.y + centre_y);
         draw.ellipse().w_h(3., 3.).xy(next_joint).color(DARKBLUE);
         next_joint = pt2(next_joint.x + 2. * centre_x, next_joint.y + 2. * centre_y);
