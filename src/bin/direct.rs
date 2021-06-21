2aaf3ce4b04d6247047a586dd9b3c1226d513f3f
Owen Brooks <owen.h.brooks@gmail.com>
2021-06-21 17:06:21 +1000

    Initial commit: beginnings of direct kinematics

---
+++ src/bin/direct.rs
@@ -0,0 +1,61 @@
+use learn_nannou::links::Link;
+use nannou::prelude::*;
+struct Model {
+    links: Vec<Link>,
+}
+fn main() {
+    nannou::app(model).event(event).simple_window(view).run();
+}
+fn model(_app: &App) -> Model {
+    let l1 = Link {
+        length: 200.,
+        angle: 0.,
+    };
+    let l2 = Link {
+        length: 100.,
+        angle: 0.,
+    };
+    Model {
+        links: vec![l1, l2],
+    }
+}
+fn event(_app: &App, model: &mut Model, event: Event) {
+    match event {
+        Event::Update(update) => {
+            let time = update.since_start.as_secs_f32();
+            let scale_factor = 0.1;
+            let new_angle = time * scale_factor % std::f32::consts::TAU;
+            for link in &mut model.links {
+                link.angle = new_angle;
+            }
+        }
+        _other => (),
+    }
+}
+fn view(app: &App, model: &Model, frame: Frame) {
+    // Prepare to draw.
+    let draw = app.draw();
+
+    // Clear the background to purple.
+    draw.background().color(CORAL);
+
+    // Draw links
+    let width = 30.;
+    let extra = 5.;
+    let mut next_joint = pt2(0., -300.);
+    for link in &model.links {
+        let centre_x = (link.length / 2. - extra) * link.angle.cos();
+        let centre_y = (link.length / 2. - extra) * link.angle.sin();
+        draw.ellipse()
+            .w_h(link.length + extra, width)
+            .color(GREENYELLOW)
+            .stroke(DARKBLUE)
+            .stroke_weight(1.)
+            .z_radians(link.angle)
+            .x_y(next_joint.x + centre_x, next_joint.y + centre_y);
+        draw.ellipse().w_h(3., 3.).xy(next_joint).color(DARKBLUE);
+        next_joint = pt2(next_joint.x + 2. * centre_x, next_joint.y + 2. * centre_y);
+    }
+
+    draw.to_frame(app, &frame).unwrap();
+}
---
+++ src/links.rs
@@ -0,0 +1,4 @@
+pub struct Link {
+    pub length: f32,
+    pub angle: f32, // radians
+}
